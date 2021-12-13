e38ccd755e7c23b533f602c40c1ad3f9982e6641
Alexander Phillips <alexphillipsdev@gmail.com>
2021-12-13 15:55:09 -0600

    connects to db before listen

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const { app } = require("./app");
+const { mongoConnect } = require("./db");
+
+(async () => {
+  await mongoConnect();
+})();
 
 app.listen(3000, () => {
   console.log("Server started SUCCESSFULLY");
